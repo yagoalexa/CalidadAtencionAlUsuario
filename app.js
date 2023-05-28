e897f15add6bef88a2b59d2a620473b2d21dd82f
Alexandra Gomez <yago_alexa@hotmail.com>
2023-05-28 13:04:01 -0500

    Variables de entorno
    Se agrega la configuración de variables de entorno, para desarrollo y producción

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
- 
+const path = require('path');
+const dotenv = require('dotenv'); 
+dotenv.config({
+	path: path.resolve(__dirname+'/enviroment', process.env.NODE_ENV + '.env')
+  });
 let http = require('http');
 let server = http.createServer();
  
@@ -8,7 +12,7 @@ function Servidor(petic, resp) {
 	resp.end();
 }
 server.on('request', Servidor);
- 
-server.listen(3000, function () {
-  	console.log('La Aplicación está funcionando en el puerto 3000');
+console.log(__dirname, process.env.NODE_ENV + '.env');
+server.listen(process.env.PORT, function () {
+  	console.log('La Aplicación está funcionando en el puerto ' + process.env.PORT);
 });
